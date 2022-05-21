cb9dc12a1111381d6245c9132a09799d6027da95
Timofey Boyko <fognir742@gmail.com>
2022-05-21 17:10:33 +0300

    Components:Themes: add button for base theme

--- src/components/Themes/index.ts
+++ src/components/Themes/index.ts
@@ -14,6 +14,84 @@ export interface ITheme {
     lineHeight: string;
     userSelect: string;
   };
+
+  button: {
+    color: {
+      base: string;
+      baseHover: string;
+      baseActive: string;
+      baseDisabled: string;
+
+      primary: string;
+      primaryHover: string;
+      primaryActive: string;
+      primaryDisabled: string;
+    };
+
+    background: {
+      base: string;
+      baseHover: string;
+      baseActive: string;
+      baseDisabled: string;
+
+      primary: string;
+      primaryHover: string;
+      primaryActive: string;
+      primaryDisabled: string;
+    };
+
+    border: {
+      base: string;
+      baseHover: string;
+      baseActive: string;
+      baseDisabled: string;
+
+      primary: string;
+      primaryHover: string;
+      primaryActive: string;
+      primaryDisabled: string;
+    };
+
+    badgeColor: {
+      base: string;
+      baseHover: string;
+      baseActive: string;
+      baseDisabled: string;
+
+      primary: string;
+      primaryHover: string;
+      primaryActive: string;
+      primaryDisabled: string;
+    };
+
+    badgeBackground: {
+      base: string;
+      baseHover: string;
+      baseActive: string;
+      baseDisabled: string;
+
+      primary: string;
+      primaryHover: string;
+      primaryActive: string;
+      primaryDisabled: string;
+    };
+
+    badgeBorder: {
+      base: string;
+      baseHover: string;
+      baseActive: string;
+      baseDisabled: string;
+
+      primary: string;
+      primaryHover: string;
+      primaryActive: string;
+      primaryDisabled: string;
+    };
+
+    borderRadius: string;
+
+    padding: string;
+  };
 }
 
 export { Base };
