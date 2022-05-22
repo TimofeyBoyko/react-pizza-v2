11fb7854017bb2a5e7fd21d66dd2b0e35e91c4d3
Timofey Boyko <fognir742@gmail.com>
2022-05-22 18:54:12 +0300

    Utils:Types: add IPizza and ICategory interfaces

---
+++ src/utils/types.ts
@@ -0,0 +1,15 @@
+export interface ICategory {
+  key: number;
+  label: string;
+}
+
+export interface IPizza {
+  id: number;
+  imageUrl: string;
+  title: string;
+  types: number[];
+  sizes: number[];
+  price: number;
+  category: number;
+  rating: number;
+}
